5e73c78757caf3c1fdbae7cba10d2d29fb09f3b7
Reza Tayebi <reza.tayebi73@gmail.com>
2024-11-18 12:47:45 +0330

    using eval

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,12 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
+    executeUserInput("console.log('Hello, world!');");
     return 'Hello World!';
   }
+}
+
+function executeUserInput(userInput) {
+  // WARNING: Using eval can lead to code injection attacks
+  eval(userInput);
 }
